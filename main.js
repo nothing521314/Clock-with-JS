f329a070b3f591bb80f69c25e74f15ba337f37c2
nothing521314 <nothing.521314@gmail.com>
2021-03-07 13:39:35 +0700

    clock with

---
+++ main.js
@@ -0,0 +1,23 @@
+const secondsHand = document.querySelector(".second-hand");
+const minutesHand = document.querySelector(".minute-hand");
+const hoursHand = document.querySelector(".hour-hand");
+
+function setDate() {
+  const timeNow = new Date();
+  const seconds = timeNow.getSeconds();
+  const secondsDegrees = ((seconds / 60) * 360 + 90);
+  secondsHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  console.log(timeNow.getSeconds());
+
+  const minutes = timeNow.getMinutes();
+  const minutesDegrees = ((minutes / 60) * 360 + 90);
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+
+  const hours = timeNow.getHours();
+  const hoursDegrees = ((hours / 60) * 360 + 45);
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+
+  console.log(timeNow);
+}
+
+setInterval(setDate, 1000);
